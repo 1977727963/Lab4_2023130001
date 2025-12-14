43915e28f7ba77fd4105165247300ab9848401d7
1977727963 <yu1977727963@gmail.com>
2025-12-14 17:26:53 +0800

    init maven project with junit tests

---
+++ src/main/java/edu/hit/lab4/Calculator.java
@@ -0,0 +1,12 @@
+package edu.hit.lab4;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+}
